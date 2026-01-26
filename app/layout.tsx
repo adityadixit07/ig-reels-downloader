0a9a0bd7fb43d205a74d82f12aa5527b22b337ac
adityadixit07 <adityadixit9660@gmail.com>
2026-01-26 09:04:44 +0530

    vercep analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -105,7 +106,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://cdninstagram.com" />
         <link rel="dns-prefetch" href="https://cdninstagram.com" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
